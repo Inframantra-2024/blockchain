4112a46a1a3bc23fd982b10e229cd9b7bec00953
Rohit <im.software@inframantra.com>
2025-07-05 16:52:04 +0530

    swagger updates

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
+const morgan = require('morgan');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
+const config = require('config');
+const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoute.js');
+const adminRoutes = require('./routes/adminRoute.js');
+const merchantRoute = require('./routes/merchantRoute.js')
+const errorHandler = require('./middleware/errorHandler.js');
+const widthdrawalRoute = require('./routes/widthdrawalRoute.js')
+const transactionRoute = require('./routes/transaction.js')
+const feeRoutes = require('./routes/feeRoute.js')
+const notificationRoutes = require('./routes/notificationRoute.js')
 
+
+const app = express();
 // External modules
 const swaggerSpec = require('./config/swaggerConfig');
 // const healthCheck = require('./middleware/healthCheck');
 
 // Routes
-const authRoutes = require('./routes/authRoute');
+// database 
 
-const app = express();
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+connectDB()
+
 
 // Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -29,7 +51,14 @@ app.get('/api-docs.json', (req, res) => {
 // app.get('/api/v1/health', healthCheck);
 
 // Routes
+// API Routes
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/admin',adminRoutes);
+app.use('/api/v1/fee',feeRoutes)
+app.use('/api/v1/merchant',merchantRoute)
+app.use('/api/v1/transaction',transactionRoute)
+app.use('/api/v1/withdrawal',widthdrawalRoute)
+app.use('/api/v1/notifications',notificationRoutes)
 
 // Default route
 app.get('/', (req, res) => {
